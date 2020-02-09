a715599a35410c545af8460f421c519900a6e29d
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 15:48:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,5 +7,10 @@ class Show < ActiveRecord::Base
     Show.find_by(rating: self.highest_rating)
   end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
 
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
 end
